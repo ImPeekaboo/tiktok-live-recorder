fdfdbe09fba9c61d5eb183dc230d574a75f9ab39
Ayam Penyet <88007539+ImPeekaboo@users.noreply.github.com>
2024-12-19 12:47:18 +0800

    Update tiktokbot.py

--- src/core/tiktokbot.py
+++ src/core/tiktokbot.py
@@ -107,13 +107,8 @@ class TikTok:
                     logger.error(Error.CONNECTION_CLOSED_AUTOMATIC)
                     time.sleep(TimeOut.CONNECTION_CLOSED * TimeOut.ONE_MINUTE)
 
-                except KeyboardInterrupt:
-                    logger.error('Stopped because keyboard interrupt.')
-                    break
-
                 except Exception as ex:
                     logger.error(ex)
-                    break
 
     def start_recording(self):
         """
